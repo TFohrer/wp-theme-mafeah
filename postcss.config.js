ad87f209103861ec7817d844cee4278873647df3
TFohrer <T.Fohrer@gmx.de>
2021-03-14 11:35:54 +0000

    Deploying to gh-pages from  @ 6289766fc140a9274753f7e6622ccc29723e03d8 🚀

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: [
-        require('stylelint'),
-        require('tailwindcss'),
-        process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-    ],
-}
